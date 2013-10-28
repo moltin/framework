26cadac3e9eef666369100f82e1e54ea265fb836
Jamie Holdroyd <apocalyps3@gmail.com>
2013-10-28 13:20:27 +0000

    Updated config

--- app/Config/config.php
+++ app/Config/config.php
@@ -1,16 +1,18 @@
 <?php
-	
-	return [
 
-		'app_name'          => 'Moltin',
+    return [
 
-		'app_mode'          => 'development',
-		'app_debug'         => true,
+        'app_name'          => 'Moltin',
 
-		'app_cookie_ttl'    => '20 minutes',
-		'app_cookie_secret' => '',
+        'app_mode'          => 'development',
+        'app_debug'         => true,
 
-		'api_client_id'     => '',
-		'api_client_secret' => ''
+        'app_per_page'      => 15,
 
-	];
+        'app_cookie_ttl'    => '20 minutes',
+        'app_cookie_secret' => '',
+
+        'api_client_id'     => '',
+        'api_client_secret' => ''
+
+    ];
